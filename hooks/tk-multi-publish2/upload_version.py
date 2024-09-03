41bb2bf2c7509d35ff2681800511b4e19149bcf4
Philipp Busch <hakkeroid@philippbusch.de>
2024-09-03 21:28:45 +0200

    Add hook file with relevant original code to remove file extension

---
+++ hooks/tk-multi-publish2/upload_version.py
@@ -0,0 +1,100 @@
+import os
+import pprint
+import sgtk
+from tank_vendor import six
+
+HookBaseClass = sgtk.get_hook_baseclass()
+
+
+class UploadVersionPlugin(HookBaseClass):
+    """
+    Plugin for sending quicktimes and images to shotgun for review.
+    """
+
+    def publish(self, settings, item):
+        """
+        Executes the publish logic for the given item and settings.
+
+        :param settings: Dictionary of Settings. The keys are strings, matching
+            the keys returned in the settings property. The values are `Setting`
+            instances.
+        :param item: Item to process
+        """
+
+        publisher = self.parent
+        path = item.properties["path"]
+
+        # allow the publish name to be supplied via the item properties. this is
+        # useful for collectors that have access to templates and can determine
+        # publish information about the item that doesn't require further, fuzzy
+        # logic to be used here (the zero config way)
+        publish_name = item.properties.get("publish_name") or item.properties.get(
+            "publish_version_name"
+        )
+        if not publish_name:
+
+            self.logger.debug("Using path info hook to determine publish name.")
+
+            # use the path's filename as the publish name
+            path_components = publisher.util.get_file_path_components(path)
+            publish_name = path_components["filename"]
+
+        self.logger.debug("Publish name: %s" % (publish_name,))
+
+        self.logger.info("Creating Version...")
+        version_data = {
+            "project": item.context.project,
+            "code": publish_name,
+            "description": item.description,
+            "entity": self._get_version_entity(item),
+            "sg_task": item.context.task,
+        }
+
+        if "sg_publish_data" in item.properties:
+            publish_data = item.properties["sg_publish_data"]
+            version_data["published_files"] = [publish_data]
+
+        if settings["Link Local File"].value:
+            version_data["sg_path_to_movie"] = path
+
+        # log the version data for debugging
+        self.logger.debug(
+            "Populated Version data...",
+            extra={
+                "action_show_more_info": {
+                    "label": "Version Data",
+                    "tooltip": "Show the complete Version data dictionary",
+                    "text": "<pre>%s</pre>" % (pprint.pformat(version_data),),
+                }
+            },
+        )
+
+        # Create the version
+        version = publisher.shotgun.create("Version", version_data)
+        self.logger.info("Version created!")
+
+        # stash the version info in the item just in case
+        item.properties["sg_version_data"] = version
+
+        thumb = item.get_thumbnail_as_path()
+
+        if settings["Upload"].value:
+            self.logger.info("Uploading content...")
+
+            # on windows, ensure the path is utf-8 encoded to avoid issues with
+            # the shotgun api
+            if sgtk.util.is_windows():
+                upload_path = six.ensure_text(path)
+            else:
+                upload_path = path
+
+            self.parent.shotgun.upload(
+                "Version", version["id"], upload_path, "sg_uploaded_movie"
+            )
+        elif thumb:
+            # only upload thumb if we are not uploading the content. with
+            # uploaded content, the thumb is automatically extracted.
+            self.logger.info("Uploading thumbnail...")
+            self.parent.shotgun.upload_thumbnail("Version", version["id"], thumb)
+
+        self.logger.info("Upload complete!")
